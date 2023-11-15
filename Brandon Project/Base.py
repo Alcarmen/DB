5c3d713ab4237b98b243cff520b15c9a4fb95a37
Alcarmen <93697943+Alcarmen@users.noreply.github.com>
2023-11-15 14:09:06 +0800

    progress 25%

--- Brandon Project/Base.py
+++ Brandon Project/Base.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,url_for,redirect
 from dbhelper import *
 
 app = Flask(__name__)
@@ -7,14 +7,26 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("Home.html")
+    
 @app.route('/Custom')
 def Custom():
     data = getall("customers")
-    head:list= ['idno','lastname','firstname','course','level', 'actions']
+    head:list= ['C_id','C_name','c_email','c_address','Active', 'actions']
+  
     return render_template("index.html", student=data, header=head)
+    
 @app.route('/Item')
 def Item():
-    return render_template("Item.html")
+    it= getall("items")
+    head:list= ['id','ISBN','Title','Author','Genre', 'Price','I_Type', 'Action']
+    return render_template("itemindex.html",item=it, header=head)
+ 
+@app.route('/create', methods=['GET','POST'])
+def create():
+    if request.method == 'POST':
+        c_id,c_name,c_email,c_address=dict(request.form).values()
+        addrecord('customers', c_id=c_id, c_name=c_name, c_email=c_email, c_address=c_address)
+    return render_template("Create.html")
 if __name__=='__main__':
     
     app.run(debug=True)
